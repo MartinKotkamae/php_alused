98446c2122f50d486ab27eee27f562c2cccbd39d
Martin <martin.kotkamae@khk.ee>
2021-03-03 11:26:48 +0200

    ülesanne 9 lahendatud

---
+++ ülesanne9.php
@@ -0,0 +1,33 @@
+<!--Ülesanne 8, Martin Kotkamäe, 03.03.2021-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Ulesanne9</title>
+    </head>
+    <body>
+      <form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        <label>Sisestage ees- ja perekonnanimi</label> <input type="text" name="kasutajan"><br>
+        <label>Sisestage siia oma sõnum</label> <input type="text" name="sonum"><br>
+        <input type="submit" value="Saada">
+      </form>
+    </body>
+</html>
+
+<?php
+$kasutajan = $_GET["kasutajan"];
+$sonum = $_GET["sonum"];
+
+echo "Tere, ".ucwords($kasutajan)."!"."<br>";
+$length = strlen($kasutajan);
+$formatted = implode(".",str_split($kasutajan));
+echo $formatted."."."<br>";
+
+$replacesona = array("noob", "ohmoon", "loll");
+$replace = "***";
+
+echo str_replace($replacesona, $replace, $sonum)."<br>";
+
+$email = array('ö', 'õ', 'ä', 'ü');
+$emAsendus = array('o', 'o', 'a', 'y');
+$khkemail = strtolower(str_replace($email, $emAsendus, $kasutajan))."@khk.ee";
+echo str_replace(' ', '.', $khkemail);
