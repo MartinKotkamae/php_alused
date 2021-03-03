060828be19715978107181108a4fb0691f2ed0b6
Martin <martin.kotkamae@khk.ee>
2021-03-03 10:58:30 +0200

    Ülesanne 6 muudatus 6.6 ulesandes

--- ülesanne6.php
+++ ülesanne6.php
@@ -23,19 +23,21 @@ for ($nr = 1; $nr <= 5; $nr++) {
     echo "<br>";
 }
 ?>
-<!--Ülesanne 6.5, Martin Kotkamäe, 09.12.2020-->
+<!--Ülesanne 6.5, Martin Kotkamäe, 02.03.2020-->
 <?php
 for ($nr = 10; $nr >= 1; $nr--) {
     echo $nr."<br>";
 }
+echo "<br>";
 ?>
 <!--Ülesanne 6.6, Martin Kotkamäe, 02.03.2021-->
 <?php
 for ($nr = 3; $nr <= 100; $nr += 3) {
     echo $nr."<br>";
 }
+echo "<br>";
 ?>
-<!--Ülesanne 6.7, Martin Kotkamäe, 09.12.2021-->
+<!--Ülesanne 6.7, Martin Kotkamäe, 02.03.2021-->
 <?php
 $poiss = array(1=> 'Tali', 'Tuli', 'Tilo');
 $tudruk = array(1=> 'Kristin','Kriev','Kriss');
